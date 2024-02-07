1bb09ec676344e354c81853c3ed453922b59fc63
wintersakura <darwinwoda@gmail.com>
2024-02-07 23:41:52 +0200

    [Fix] add db tags

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Transaction struct {
-	Date       time.Time `json:"date"`
-	CreatedAt  time.Time `json:"createdAt"`
-	UpdatedAt  time.Time `json:"updatedAt"`
-	UserID     string    `json:"userId"`
-	Type       Type      `json:"type"`
-	Note       string    `json:"note"`
+	Date       time.Time `json:"date"       db:"date"`
+	CreatedAt  time.Time `json:"createdAt"  db:"created_at"`
+	UpdatedAt  time.Time `json:"updatedAt"  db:"updated_at"`
+	UserID     string    `json:"userId"     db:"user_id"`
+	Type       Type      `json:"type"       db:"type"`
+	Note       string    `json:"note"       db:"note"`
 	Currency   Currency  `json:"currency"`
-	Amount     float64   `json:"amount"`
-	ID         uuid.UUID `json:"id"`
-	CategoryID uuid.UUID `json:"categoryId"`
+	Amount     float64   `json:"amount"     db:"amount"`
+	ID         uuid.UUID `json:"id"         db:"id"`
+	CategoryID uuid.UUID `json:"categoryId" db:"category_id"`
 }
