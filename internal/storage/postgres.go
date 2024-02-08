8bcb1660a6913b3a3f8090347475d90d1bc7e0c5
wintersakura <darwinwoda@gmail.com>
2024-02-08 19:16:22 +0200

    [Fix] add timeout for db ping

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -1,6 +1,9 @@
 package storage
 
 import (
+	"context"
+	"time"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -10,7 +13,10 @@ func NewPostgresDB(connStr string) (*sqlx.DB, error) {
 		return nil, err
 	}
 
-	err = db.Ping()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
 	if err != nil {
 		return nil, err
 	}
